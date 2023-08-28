8dd0663df240275285bdaae01436caff6c56a1b1
PasqualeIsabelli <pasquale0395@gmail.com>
2023-08-28 15:01:52 +0200

    add initial files

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script>
+import TheHeader from './components/TheHeader.vue';
+import TheFooter from './components/TheFooter.vue';
+
+export default {
+  components: {
+    TheHeader,
+    TheFooter
+  },
+  data() {
+    return {}
+  }
+}
+</script>
+
+
+<template>
+  
+</template>
+
+
+<style lang="scss" scoped>
+@use './styles/partials/variables' as *;
+@use './styles/partials/mixins' as *;
+
+
+
+</style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+  data() {
+    return {}
+  }
+}
+</script>
+
+
+<template>
+  
+</template>
+
+
+<style lang="scss" scoped>
+@use './styles/partials/variables' as *;
+@use './styles/partials/mixins' as *;
+
+
+
+</style>
